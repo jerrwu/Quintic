feac0d36a60a9fab9e6445fd2a7de676fa3f0e4a
jerrwu <48305556+jerrwu@users.noreply.github.com>
2020-01-24 21:27:31 -0500

    fixed bug with stringhelper

--- app/src/main/java/com/jerrwu/quintic/helpers/StringHelper.kt
+++ app/src/main/java/com/jerrwu/quintic/helpers/StringHelper.kt
@@ -20,7 +20,7 @@ object StringHelper {
             in 13 downTo 11 -> getString(R.string.midday, context)
             in 17 downTo 14 -> getString(R.string.afternoon, context)
             in 20 downTo 18 -> getString(R.string.evening, context)
-            in 23 downTo 22 -> getString(R.string.night, context)
+            in 23 downTo 21 -> getString(R.string.night, context)
             24 -> getString(R.string.late_night, context)
             else -> "hie..."
         }
@@ -34,7 +34,7 @@ object StringHelper {
             in 13 downTo 11 -> getString(R.string.greeting_lunch, context)
             in 17 downTo 14 -> getString(R.string.greeting_afternoon, context)
             in 20 downTo 18 -> getString(R.string.greeting_evening, context)
-            in 23 downTo 22 -> getString(R.string.greeting_night, context)
+            in 23 downTo 21 -> getString(R.string.greeting_night, context)
             24 -> getString(R.string.greeting_late_night, context)
             else -> "hie..."
         }
