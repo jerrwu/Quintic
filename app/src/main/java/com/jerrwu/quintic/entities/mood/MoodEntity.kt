fc6f8d61154c15948878cadfd2f43b1010514d03
Jerry Wu <jerrywu21600@gmail.com>
2020-01-30 14:06:52 -0500

    added angry mood

--- app/src/main/java/com/jerrwu/quintic/entities/mood/MoodEntity.kt
+++ app/src/main/java/com/jerrwu/quintic/entities/mood/MoodEntity.kt
@@ -5,11 +5,12 @@ import com.jerrwu.quintic.R
 class MoodEntity(var id: Int, var icOutline: Int, var icFilled: Int, var color: Int) {
     companion object {
         val NONE = MoodEntity(0, 0, 0, 0)
-        val VERY_BAD = MoodEntity(1, R.drawable.ic_cry_outline, R.drawable.ic_cry, R.color.red)
+        val VERY_BAD = MoodEntity(1, R.drawable.ic_cry_outline, R.drawable.ic_cry, R.color.purple)
         val BAD = MoodEntity(2, R.drawable.ic_sad_outline, R.drawable.ic_sad, R.color.orange)
         val NEUTRAL = MoodEntity(3, R.drawable.ic_neutral_outline, R.drawable.ic_neutral, R.color.yellow)
         val GOOD = MoodEntity(4, R.drawable.ic_happy_outline, R.drawable.ic_happy, R.color.blue)
         val VERY_GOOD = MoodEntity(5, R.drawable.ic_excited_outline, R.drawable.ic_excited, R.color.green)
+        val ANGRY = MoodEntity(6, R.drawable.ic_angry_outline, R.drawable.ic_angry, R.color.red)
 
         fun parse(mood: Int?): MoodEntity {
             return when (mood) {
@@ -18,6 +19,7 @@ class MoodEntity(var id: Int, var icOutline: Int, var icFilled: Int, var color: 
                 NEUTRAL.id -> NEUTRAL
                 GOOD.id -> GOOD
                 VERY_GOOD.id -> VERY_GOOD
+                ANGRY.id -> ANGRY
                 else -> NONE
             }
         }
