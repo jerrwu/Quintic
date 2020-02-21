1cb9d6ecc3db1f354b7b16fe5f1cb8373582f8df
Jerry Wu <jerrywu21600@gmail.com>
2020-02-21 15:27:33 -0500

    updated version number

--- app/src/main/java/com/jerrwu/quintic/common/constants/Constants.kt
+++ app/src/main/java/com/jerrwu/quintic/common/constants/Constants.kt
@@ -9,8 +9,7 @@ object Constants {
     const val TAG_DELIMITER = "+"
 
     const val CHANGELOG =
-        "- Added Dec 2019, Jan 2020, Mar 2020, Apr 2020, May 2020 in the calendar. \n\n " +
-                "- Updated the way preferences are handled. \n\n" +
-                "- Added changelog display after updating. \n\n" +
-                "- Added launcher shortcuts."
+        "- Added launcher shortcuts. \n\n " +
+                "- Added tags. \n\n" +
+                "- Fixes for AccountActivity."
 }
