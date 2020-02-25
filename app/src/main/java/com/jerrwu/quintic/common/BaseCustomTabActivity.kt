76ce14c31867fb147c6fdd0dd638c9d1c1c64344
Jerry Wu <jerrywu21600@gmail.com>
2020-02-25 16:04:17 -0500

    abstracted custom tab activity

---
+++ app/src/main/java/com/jerrwu/quintic/common/BaseCustomTabActivity.kt
@@ -0,0 +1,31 @@
+package com.jerrwu.quintic.common
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import com.jerrwu.quintic.R
+
+abstract class BaseCustomTabActivity : BaseActivity() {
+    companion object {
+        val TAG = BaseCustomTabActivity::class.java.simpleName
+    }
+
+    protected var url: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val uri = Uri.parse(url)
+
+        CustomTabsIntent.Builder()
+            .setToolbarColor(
+                ContextCompat.getColor(this,
+                    R.color.colorPrimary
+                ))
+            .setShowTitle(true)
+            .build()
+            .launchUrl(this, uri)
+        finish()
+    }
+}
--- app/src/main/java/com/jerrwu/quintic/common/GithubTabActivity.kt
+++ app/src/main/java/com/jerrwu/quintic/common/GithubTabActivity.kt
@@ -7,22 +7,13 @@ import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import com.jerrwu.quintic.R
 
-class GithubTabActivity : AppCompatActivity() {
+class GithubTabActivity : BaseCustomTabActivity() {
     companion object {
         val TAG = GithubTabActivity::class.java.simpleName
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        url = getString(R.string.github_link_main)
         super.onCreate(savedInstanceState)
-
-        val uri = Uri.parse(getString(R.string.github_link_main))
-        val intentBuilder = CustomTabsIntent.Builder()
-        intentBuilder.setToolbarColor(ContextCompat.getColor(this,
-            R.color.colorPrimary
-        ))
-        intentBuilder.setShowTitle(true)
-        val customTabsIntent = intentBuilder.build()
-        customTabsIntent.launchUrl(this, uri)
-        finish()
     }
 }
